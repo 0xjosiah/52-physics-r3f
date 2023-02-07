27633dfbcc298e155fa8bfa6109f82746d02867a
Josiah Webb <jj.webb520@gmail.com>
2023-02-06 17:40:07 -0700

    adjusts walls positions

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -161,10 +161,10 @@ export default function Experience()
 
             {/* walls */}
             <RigidBody type='fixed'>
-                <CuboidCollider args={[ 5, 2, .5 ]} position={[ 0, 1, 5.25 ]} />
-                <CuboidCollider args={[ 5, 2, .5 ]} position={[ 0, 1, -5.25 ]} />
-                <CuboidCollider args={[ .5, 2, 5 ]} position={[ 5.25, 1, 0 ]} />
-                <CuboidCollider args={[ .5, 2, 5 ]} position={[ -5.25, 1, 0 ]} />
+                <CuboidCollider args={[ 5, 2, .5 ]} position={[ 0, 1, 5.5 ]} />
+                <CuboidCollider args={[ 5, 2, .5 ]} position={[ 0, 1, -5.5 ]} />
+                <CuboidCollider args={[ .5, 2, 5 ]} position={[ 5.5, 1, 0 ]} />
+                <CuboidCollider args={[ .5, 2, 5 ]} position={[ -5.5, 1, 0 ]} />
             </RigidBody>
 
 
