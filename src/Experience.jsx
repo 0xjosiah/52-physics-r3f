b8666c051b3d040ae39d0db544883b94fad6a26f
Josiah Webb <jj.webb520@gmail.com>
2023-02-06 17:38:04 -0700

    creates walls to box models in

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -159,6 +159,14 @@ export default function Experience()
                 <primitive object={ scene } scale={ .25 } />
             </RigidBody>
 
+            {/* walls */}
+            <RigidBody type='fixed'>
+                <CuboidCollider args={[ 5, 2, .5 ]} position={[ 0, 1, 5.25 ]} />
+                <CuboidCollider args={[ 5, 2, .5 ]} position={[ 0, 1, -5.25 ]} />
+                <CuboidCollider args={[ .5, 2, 5 ]} position={[ 5.25, 1, 0 ]} />
+                <CuboidCollider args={[ .5, 2, 5 ]} position={[ -5.25, 1, 0 ]} />
+            </RigidBody>
+
 
         </Physics>
 
